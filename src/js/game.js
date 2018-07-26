dd02cab6c6525862330360225cfe580adc10ca8b
Oliver Mascarenhas <oliver.savio@gmail.com>
2018-07-26 20:14:18 +0530

    set up paddle and collision with game over

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 });
 
 var ball;
+var paddle;
 
 
 function preload() {
@@ -11,14 +12,33 @@ function preload() {
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#eee';
   game.load.image('ball', 'img/ball.png');
+  game.load.image('paddle', 'img/paddle.png');
 }
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(game.world.width * 0.5, game.world.height - 25, 'ball');
+
+  game.physics.arcade.checkCollision.down = false;
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-  ball.body.velocity.set(120, 120);
+
+  ball.checkWorldBounds = true;
+  ball.events.onOutOfBounds.add(function () {
+    alert('Game over!');
+    location.reload();
+  }, this);
+  ball.body.velocity.set(150, -150);
+  ball.body.collideWorldBounds = true;
+  ball.body.bounce.set(1);
+
+
+  paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
+  paddle.anchor.set(0.5, 1);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true;
+
+
 }
 function update() {
-  ball.x += 1;
-  ball.y += 1;
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width * 0.5;
 }

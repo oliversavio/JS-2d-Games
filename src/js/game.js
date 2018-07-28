d560c625ab2b7495c5897f8fefc6abd21d85a37b
Oliver Mascarenhas <oliver.savio@gmail.com>
2018-07-28 16:43:57 +0530

    refactor code

--- src/js/game.js
+++ src/js/game.js
@@ -19,18 +19,26 @@ var handleGameOver = function () {
   location.reload();
 };
 
-function create() {
-  game.physics.startSystem(Phaser.Physics.ARCADE);
+var initBall = function () {
   ball = game.add.sprite(game.world.width * 0.5, game.world.height - 25, 'ball');
-
+  //Done bounce at bottom
   game.physics.arcade.checkCollision.down = false;
+  //Enable Physics
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-
+  //Make ball bounce
   ball.checkWorldBounds = true;
+  //Handle game over
   ball.events.onOutOfBounds.add(handleGameOver, this);
+
   ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
+};
+
+function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  initBall();
 
   paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
   paddle.anchor.set(0.5, 1);
@@ -42,3 +50,5 @@ function update() {
   game.physics.arcade.collide(ball, paddle);
   paddle.x = game.input.x || game.world.width * 0.5;
 }
+
+

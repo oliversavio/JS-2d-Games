c3e098d2cbd10ad5b9bd088c62c8c7e39f10e2c4
Oliver Mascarenhas <oliver.savio@gmail.com>
2018-08-22 20:55:01 +0530

    add random balls to canvas

--- src/js/dodge.js
+++ src/js/dodge.js
@@ -27,23 +27,25 @@ var handleGameOver = function () {
   location.reload();
 };
 
-var genRandomVelocity = function() {
+var genRandomVelocity = function () {
   return Math.floor(Math.random() * (velocityMax - velocityMin + 1)) + velocityMin;
 };
+
+
 
 var initBall = function () {
   ballInfo = {
     count: 10,
     offset: 40,
     cords: { x: 40, y: -20 },
-    levels: [{c: 5}, {c: 7},{c: 8},{c: 10}]
+    levels: [{ c: 2 }, { c: 3 }, { c: 5 }, { c: 7 }, { c: 9 },{ c: 10 }]
   }
 
   balls = game.add.group();
   balls.enableBody = true;
   balls.physicsBodyType = Phaser.Physics.ARCADE;
 
-  for(var i = 0 ; i < ballInfo.count ; i++) {
+  for (var i = 0; i < ballInfo.count; i++) {
     x = ((i * ballInfo.offset) + ballInfo.cords.x);
     y = ballInfo.cords.y;
     var ball = balls.create(x, y, 'ball');
@@ -55,31 +57,30 @@ var initBall = function () {
 
 };
 
-var increaseLevel = function(){
-  if(level > ballInfo.levels.length) {
+var increaseLevel = function () {
+  if (level > ballInfo.levels.length) {
     return;
   }
   var number = ballInfo.levels[level - 1];
-  for(var i = 0 ; i < number.c ; i++) {
-    var b = balls.getAt(i);
+  for (var i = 0; i < number.c; i++) {
+    var ballIndex = Math.floor(Math.random() * (ballInfo.count)) + 0;
+    var b = balls.getAt(ballIndex);
     b.body.velocity.set(0, genRandomVelocity());
   }
   level += 1;
   levelText.setText("Level: " + level);
 };
 
-var redrawBall = function(ball) {
+var redrawBall = function (ball) {
   ball.reset(ball.x, 0);
   ball.body.velocity.set(0, genRandomVelocity());
 
   score += 10;
-  if (score % 30 == 0) {
+  if (score % 40 == 0) {
     increaseLevel();
   }
   scoreText.setText('Points: ' + score);
 };
-
-
 
 var initStartButton = function () {
   startButton = game.add.button(game.world.width * 0.5, game.world.height * 0.5, 'button', startGame, this, 1, 0, 2);
@@ -93,20 +94,16 @@ var startGame = function () {
   increaseLevel();
 };
 
-
-
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  //initBall();
   initStartButton();
-  //startGame(); //TODO remove this later
   scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
   levelText = game.add.text(game.world.width - 70, 5, "Level: 1", textStyle);
 }
 
 
 function update() {
-  
+
 
 }
 
